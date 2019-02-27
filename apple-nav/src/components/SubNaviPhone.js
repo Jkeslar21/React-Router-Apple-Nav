25294df21aa99e46cca51987cc4d72504811fdc7
jkeslar21 <diamondroad91@gmail.com>
2019-02-27 16:21:26 -0700

    iphoneSubNav

--- apple-nav/src/components/SubNaviPhone.js
+++ apple-nav/src/components/SubNaviPhone.js
@@ -4,42 +4,42 @@ export default function SubNaviPhone() {
   return (
     <>
       <div className='iphone'>
-        <div className='newLogo'>
+        <div className='newLogo iphonePad'>
           <div className='sub-nav-styling'>
-            <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphonexs_large.svg' />
+            <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphonexs_large.svg' className='xs' />
             <p>iPhone XS</p>
           </div>
           <p className='new'>New</p>
         </div>
-        <div className='newLogo'>
+        <div className='newLogo iphonePad'>
           <div className='sub-nav-styling'>
-            <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphonexr_large.svg' />
+            <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphonexr_large.svg' className='xr' />
             <p>iPhone XR</p>
           </div>
           <p className='new'>New</p>
         </div>
-        <div className='sub-nav-styling'>
-          <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphone8_large.svg' />
+        <div className='sub-nav-styling iphonePad'>
+          <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphone8_large.svg' className='eight'/>
           <p>iPhone 8</p>
         </div>
-        <div className='sub-nav-styling'>
-          <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphone7_large.svg' />
+        <div className='sub-nav-styling iphonePad'>
+          <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphone7_large.svg' className='seven' />
           <p>iPhone 7</p>
         </div>
-        <div className='sub-nav-styling'>
+        <div className='sub-nav-styling iphonePad'>
           <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/ios_large.svg' />
           <p>iOS 12</p>
         </div>
-        <div className='sub-nav-styling'>
-          <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/airpods_large.svg' />
+        <div className='sub-nav-styling iphonePad'>
+          <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/airpods_large.svg' className='airpod' />
           <p>AirPods</p>
         </div>
-        <div className='sub-nav-styling mini'>
-            <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/accessories_large.svg' className='miniResize' />
+        <div className='sub-nav-styling iphonePad'>
+            <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/accessories_large.svg' className='accessiphone' />
             <p>Accessories</p>
         </div>
-        <div className='sub-nav-styling'>
-          <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/compare_large.svg' />
+        <div className='sub-nav-styling iphonePad'>
+          <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/compare_large.svg' className='compareiphone' />
           <p>Compare</p>
         </div>
       </div>
