73cea0f8e7e3e01ffaf71550266a1f0427b2c987
jkeslar21 <diamondroad91@gmail.com>
2019-02-26 19:29:48 -0700

    svg nav

--- apple-nav/src/components/Nav.js
+++ apple-nav/src/components/Nav.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 export default function Nav() {
   return (
     <div className='nav'>
-      <Link to='/'><i class="fab fa-apple fa-lg nav-link"></i></Link>
-      <Link to='/mac' className='nav-link'>Mac</Link>
-      <Link to='/ipad' className='nav-link'>iPad</Link>
-      <Link to='/' className='nav-link'>iPhone</Link>
-      <Link to='/' className='nav-link'>Watch</Link>
-      <Link to='/' className='nav-link'>TV</Link>
-      <Link to='/' className='nav-link'>Music</Link>
-      <Link to='/' className='nav-link'>Support</Link>
-      <Link to='/' className='nav-link'><i class="fas fa-search fa-lg"></i></Link>
-      <Link to='/' className='nav-link'><i class="far fa-clipboard fa-lg"></i></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/apple/image_large.svg' className='nav-link' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/mac/image_large.svg' className='nav-link' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/ipad/image_large.svg' className='nav-link' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/iphone/image_large.svg' className='nav-link' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/watch/image_large.svg' className='nav-link' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/tv/image_large.svg' className='nav-link' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/music/image_large.svg' className='nav-link' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/support/image_large.svg' className='nav-link' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/search/image_large.svg' className='nav-link' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/bag/image_large.svg' className='nav-link' /></Link>
     </div>
   )
 }
--- apple-nav/src/components/SubNavMac.js
+++ apple-nav/src/components/SubNavMac.js
@@ -8,9 +8,12 @@ export default function SubNavMac() {
           <img src='https://www.apple.com/v/mac/home/ae/images/familybrowser/macbook__fjd32xo0xwmm_large.svg' />
           <p>MacBook</p>
         </div>
+        <div className='newLogo macbookAir'>
         <div className='sub-nav-styling'>
           <img src='https://www.apple.com/v/mac/home/ae/images/familybrowser/macbookair__by1vem70tkc2_large.svg' />
           <p>MacBook Air</p>
+        </div>
+          <p className='new'>New</p>
         </div>
         <div className='sub-nav-styling'>
           <img src='https://www.apple.com/v/mac/home/ae/images/familybrowser/macbookpro__e0wcugzmt26a_large.svg' />
@@ -28,9 +31,12 @@ export default function SubNavMac() {
           <img src='https://www.apple.com/v/mac/home/ae/images/familybrowser/macpro__dt69qr0ywncm_large.svg' />
           <p>Mac Pro</p>
         </div>
+        <div className='newLogo macMini'>
         <div className='sub-nav-styling mini'>
-          <img src='https://www.apple.com/v/mac/home/ae/images/familybrowser/macmini__rzlk7b0xsmqq_large.svg' />
+          <img src='https://www.apple.com/v/mac/home/ae/images/familybrowser/macmini__rzlk7b0xsmqq_large.svg' className='miniResize' />
           <p>Mac mini</p>
+        </div>
+        <p className='new macMiniNew'>New</p>
         </div>
         <div className='sub-nav-styling'>
           <img src='https://www.apple.com/v/mac/home/ae/images/familybrowser/mac_acc__fia3ihqup4ae_large.svg' />
