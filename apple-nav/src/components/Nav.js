54aab79f46d2291561af8afac55be6960b9c985e
jkeslar21 <diamondroad91@gmail.com>
2019-02-27 23:28:07 -0700

    iphoneSubNavDone

--- apple-nav/src/components/Nav.js
+++ apple-nav/src/components/Nav.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 export default function Nav() {
   return (
     <div className='nav'>
-      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/apple/image_large.svg' className='nav-link' /></Link>
-      <Link to='/mac'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/mac/image_large.svg' className='nav-link' /></Link>
-      <Link to='/ipad'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/ipad/image_large.svg' className='nav-link' /></Link>
-      <Link to='/iphone'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/iphone/image_large.svg' className='nav-link' /></Link>
-      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/watch/image_large.svg' className='nav-link' /></Link>
-      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/tv/image_large.svg' className='nav-link' /></Link>
-      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/music/image_large.svg' className='nav-link' /></Link>
-      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/support/image_large.svg' className='nav-link' /></Link>
-      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/search/image_large.svg' className='nav-link' /></Link>
-      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/bag/image_large.svg' className='nav-link' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/apple/image_large.svg' className='nav-link navLogo' /></Link>
+      <Link to='/mac'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/mac/image_large.svg' className='nav-link navMac' /></Link>
+      <Link to='/ipad'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/ipad/image_large.svg' className='nav-link naviPad' /></Link>
+      <Link to='/iphone'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/iphone/image_large.svg' className='nav-link naviPhone' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/watch/image_large.svg' className='nav-link navWatch' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/tv/image_large.svg' className='nav-link navTV' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/music/image_large.svg' className='nav-link navMusic' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/links/support/image_large.svg' className='nav-link navSupport' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/search/image_large.svg' className='nav-link navSearch' /></Link>
+      <Link to='/'><img src='https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/bag/image_large.svg' className='nav-link navBag' /></Link>
     </div>
   )
 }
--- apple-nav/src/components/SubNaviPhone.js
+++ apple-nav/src/components/SubNaviPhone.js
@@ -4,41 +4,41 @@ export default function SubNaviPhone() {
   return (
     <>
       <div className='iphone'>
-        <div className='newLogo iphonePad'>
+        <div className='newLogo iphonePad xS-Position'>
           <div className='sub-nav-styling'>
             <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphonexs_large.svg' className='xs' />
-            <p>iPhone XS</p>
+            <p>iPhone X<span className='s-Sizing'>S</span></p>
           </div>
           <p className='new'>New</p>
         </div>
-        <div className='newLogo iphonePad'>
+        <div className='newLogo iphonePad xR-Position'>
           <div className='sub-nav-styling'>
             <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphonexr_large.svg' className='xr' />
-            <p>iPhone XR</p>
+            <p>iPhone X<span className='r-Sizing'>R</span></p>
           </div>
           <p className='new'>New</p>
         </div>
-        <div className='sub-nav-styling iphonePad'>
+        <div className='sub-nav-styling iphonePad eightPosition'>
           <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphone8_large.svg' className='eight'/>
           <p>iPhone 8</p>
         </div>
-        <div className='sub-nav-styling iphonePad'>
+        <div className='sub-nav-styling iphonePad sevenPosition'>
           <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/iphone7_large.svg' className='seven' />
           <p>iPhone 7</p>
         </div>
-        <div className='sub-nav-styling iphonePad'>
+        <div className='sub-nav-styling iphonePad iOS-Position'>
           <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/ios_large.svg' />
           <p>iOS 12</p>
         </div>
-        <div className='sub-nav-styling iphonePad'>
+        <div className='sub-nav-styling iphonePad aPodPosition'>
           <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/airpods_large.svg' className='airpod' />
           <p>AirPods</p>
         </div>
-        <div className='sub-nav-styling iphonePad'>
+        <div className='sub-nav-styling iphonePad accessPosition'>
             <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/accessories_large.svg' className='accessiphone' />
             <p>Accessories</p>
         </div>
-        <div className='sub-nav-styling iphonePad'>
+        <div className='sub-nav-styling iphonePad iphoneComparePosition'>
           <img src='https://www.apple.com/v/iphone/home/z/images/chapternav/compare_large.svg' className='compareiphone' />
           <p>Compare</p>
         </div>
